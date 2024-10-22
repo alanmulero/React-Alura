c61fa6e8b53aa82fcbc3df18f38fccf78d9fbbf7
alanmulero <alan.mulero@gmail.com>
2024-10-21 21:59:27 -0300

    escutando o input e validando

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,10 +2,15 @@
 import './CampoTexto.css'
 const CampoTexto = (props) => {
     //const placeHolderModificada = `${props.placeholder}...`;
+    // props.obrigatorio pega validação do html. Validação nativa combinada com React
+    let valorDigitado = 'Jujuba';
+    const quandoDigitado = (evento) => {
+        valorDigitado = evento.target.value;
+        console.log(valorDigitado)} // capturando input
     return(
         <div className="campoTexto">
             <label>{props.label}</label>
-            <input  placeholder={`${props.placeholder}...`}  />
+            <input value={valorDigitado} onChange={quandoDigitado}  required={props.obrigatorio} placeholder={`${props.placeholder}...`}  />
         </div>
     )
 
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,15 +5,22 @@ import Botao from "../Botao";
 
 const Formulario = () => {
     const paises = ['África','América do Norte','América do Sul','Ásia','Oceania']
+    const aoSalvar = (evento) => {
+        evento.preventDefault();// Alterando comportamento padrão.
+        console.log('Formulario submetido');
+    }
+
+
+
     return (
         <section className="formulario">
 
-            <form className="formulario-form">
+            <form onSubmit={aoSalvar} className="formulario-form">
                 <h2>Preencha o formulário de cadastro</h2>
-                <CampoTexto label="Nome" placeholder={"Digite o seu nome"}/>
-                <CampoTexto label={"Cargo"} placeholder={"Digite o seu cargo"}/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder={"Digite o seu nome"}/>
+                <CampoTexto obrigatorio={true} label={"Cargo"} placeholder={"Digite o seu cargo"}/>
                 <CampoTexto label={"Empresa"} placeholder={"Digite o nome da empresa"}/>
-                <CampoTexto label={"Atividade"} placeholder={"Digite o ramo de atividade da empresa"}/>
+                <CampoTexto  label={"Atividade"} placeholder={"Digite o ramo de atividade da empresa"}/>
                 <ListaSuspensa label={'Selecione o continente:'} itens={paises}/>
                 <Botao>
                     Criar Card
