2e15d9956907a59a5caeb85090a49a6f44f16628
alanmulero <alan.mulero@gmail.com>
2024-10-25 10:42:34 -0300

    Cadastro para cards ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
 
+import { useState } from 'react';
 import Banner from './componentes/Banner/Index';
 import Formulario from "./componentes/Formulario";
 
 function App() {
+
+  // Criando um estado.
+  const [colaboradores, setColaboradors] = useState([]); // useStat de um array vazio. Guardando o colaborador cadastrado.
+
+  // Função para capturar colaborador.
+  const colaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+  }
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      {/*Funçao para capturar colaborador. colaborador passa adiante para colaboradoAdicionado  */}
+      <Formulario  aoColaboradorCadastrado={item => colaboradorAdicionado(item)}/>
 
     </div>
   );
